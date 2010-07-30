d9ca393d6fdb4683edb24aeda9ad718ec457ae3c
David Dalrymple <davidad@alum.mit.edu>
2010-07-30 00:30:08 -0700

    Adding problem-spaces EP and fetch script

---
+++ fetch_eps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://su-etherpad.com/ep/pad/export/space-exp-tech-overview/latest?format=txt -O et-overview.tex
+wget http://su-etherpad.com/ep/pad/export/space-grand-challenges/latest?format=txt -O problem-spaces.tex
