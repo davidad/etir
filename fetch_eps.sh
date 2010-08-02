e5dc9d72e38314f646a407ef25ac4fad4711a010
David Dalrymple <davidad@alum.mit.edu>
2010-08-02 16:55:39 -0700

    OH GOD ONLY HALF AN HOUR LEFT OH GOD

--- fetch_eps.sh
+++ fetch_eps.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-wget http://su-etherpad.com/ep/pad/export/space-exp-tech-overview/latest?format=txt -O et-overview.tex
-wget http://su-etherpad.com/ep/pad/export/space-grand-challenges/latest?format=txt -O problem-spaces.tex
+#wget http://su-etherpad.com/ep/pad/export/space-exp-tech-overview/latest?format=txt -O et-overview.tex
+#wget http://su-etherpad.com/ep/pad/export/space-grand-challenges/latest?format=txt -O problem-spaces.tex
+wget http://su-etherpad.com/ep/pad/export/space-etir-ideas/latest?format=txt -O chrisographs.tex
+wget http://su-etherpad.com/ep/pad/export/space-bib/latest?format=txt -O space-tp.bib
+wget http://su-etherpad.com/ep/pad/export/space-glossary/latest?format=txt -O glossary.tex
